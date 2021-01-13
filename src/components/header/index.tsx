bf3bfdc68761f8a8707a515686aa3bc4d630a2ba
zhangyinge <364846657@qq.com>
2021-01-13 15:15:50 +0800

    fix: header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,18 @@ import * as React from 'react';
 import styles from './index.less';
 
 export const Header = () => {
+  const [exitvisible, setExit] = React.useState(false);
+  const handleAvatarClick = React.useCallback(() => {
+    setExit((v) => !v);
+  }, []);
   return (
-    <div>
+    <div className={styles['header']}>
       <span className={styles['logo']} />
-      <div>
+      <div className={styles['system-title']}>
         企业项目管理系统<span>个人端</span>
+      </div>
+      <div className={styles['avatar']} onClick={handleAvatarClick}>
+        {exitvisible && <span className={styles['exit']}>退出登陆</span>}
       </div>
     </div>
   );
